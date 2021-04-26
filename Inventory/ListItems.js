4d6d393ba815cd48918972a8f99705491fc1d87f
Rysarian <rysarian1@gmail.com>
2021-04-26 15:28:32 -0700

    Adjusted price on linked items

--- Inventory/ListItems.js
+++ Inventory/ListItems.js
@@ -15,7 +15,7 @@ for (let i = 0; i < selected_items.length; i++) {
     let item = selected_items[i];
     let linked_item = all_items.filter(i => i.name === item.name);
     if (linked_item.length > 0) {
-        optionsText += `<tr><td>${linked_item[0].link}</td><td>${item.data.data.price.value}</td></tr>`;
+        optionsText += `<tr><td>${linked_item[0].link}</td><td>${linked_item[0].data.data.price.value}</td></tr>`;
     } else {
         optionsText += `<tr><td>${item.name}</td><td>${item.data.data.price.value}</td></tr>`;
     }    

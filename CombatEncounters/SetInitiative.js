6a01dbd624e1a68ea7f021ed4623e16c5e1826f0
Rysarian <rysarian1@gmail.com>
2022-02-07 00:03:12 -0800

    Update SetInitiative.js

--- CombatEncounters/SetInitiative.js
+++ CombatEncounters/SetInitiative.js
@@ -1,14 +1,17 @@
 const cmbt = game.combat;
 function hasPlayer(c) {
-  if (c._token?.data?.disposition === 1) {
-    return { _id: c.id, img: "systems/starwarsffg/images/dice/genesys/lightside.png", name: "Friendly" };
-  }
-  else if (c._token?.data?.disposition === 0) {
-    return { _id: c.id, img: "systems/starwarsffg/images/mod-all.png", name: "Neutral" };
-  }
-  else {
-    return { _id: c.id, img: "systems/starwarsffg/images/dice/genesys/darkside.png", name: "Hostile" };
-  }
+    if (c.hasPlayerOwner) {
+        return { _id: c.id, img: "icons/magic/symbols/chevron-elipse-circle-blue.webp", name: "PC" };
+    }
+    else if (c.token?.data?.disposition === 1) {
+        return { _id: c.id, img: "systems/starwarsffg/images/dice/genesys/lightside.png", name: "Friendly" };
+    }
+    else if (c.token?.data?.disposition === 0) {
+        return { _id: c.id, img: "systems/starwarsffg/images/mod-all.png", name: "Neutral" };
+    }
+    else {
+        return { _id: c.id, img: "systems/starwarsffg/images/dice/genesys/darkside.png", name: "Hostile" };
+    }
 
 }
 let updates = {};
